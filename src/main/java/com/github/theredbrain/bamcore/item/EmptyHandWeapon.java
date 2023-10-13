e1c9b297ee4b62cd087f80af0497fe855254e771
TheRedBrain <thetrueredbrain@gmail.com>
2023-10-13 16:10:26 +0200

    fixed empty hand weapons

--- src/main/java/com/github/theredbrain/bamcore/item/EmptyHandWeapon.java
+++ src/main/java/com/github/theredbrain/bamcore/item/EmptyHandWeapon.java
@@ -1,31 +1,16 @@
 package com.github.theredbrain.bamcore.item;
 
 import com.github.theredbrain.bamcore.api.item.BetterAdventureMode_BasicWeaponItem;
-import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.Multimap;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.damage.DamageType;
 import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ToolItem;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
 public class EmptyHandWeapon extends BetterAdventureMode_BasicWeaponItem {
-    private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
 
     public EmptyHandWeapon(float attackDamage, float attackSpeed, int staminaCost, Settings settings) {
-        super(DamageTypes.PLAYER_ATTACK, null, 7, -3.5F, staminaCost, 0, settings);
-        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
-        builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Weapon modifier", (double)attackDamage, EntityAttributeModifier.Operation.ADDITION));
-        builder.put(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Weapon modifier", (double)attackSpeed, EntityAttributeModifier.Operation.ADDITION));
-        this.attributeModifiers = builder.build();
+        super(DamageTypes.PLAYER_ATTACK, null, attackDamage, attackSpeed, staminaCost, 0, settings);
     }
 
     @Override
@@ -36,13 +21,5 @@ public class EmptyHandWeapon extends BetterAdventureMode_BasicWeaponItem {
     @Override
     public boolean isDamageable() {
         return false;
-    }
-
-    @Override
-    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
-        if (slot == EquipmentSlot.MAINHAND || slot == EquipmentSlot.OFFHAND) {
-            return this.attributeModifiers;
-        }
-        return super.getAttributeModifiers(slot);
     }
 }
