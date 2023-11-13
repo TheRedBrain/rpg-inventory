c23de3d9e17e08c08f4f7b007077b09c12c3e231
TheRedBrain <thetrueredbrain@gmail.com>
2023-11-13 18:07:08 +0100

    creative players can now access the creative housing screen by interacting with the block

--- src/main/java/com/github/theredbrain/bamcore/block/HousingBlock.java
+++ src/main/java/com/github/theredbrain/bamcore/block/HousingBlock.java
@@ -1,6 +1,9 @@
 package com.github.theredbrain.bamcore.block;
 
+import com.github.theredbrain.bamcore.block.entity.AreaFillerBlockBlockEntity;
 import com.github.theredbrain.bamcore.block.entity.HousingBlockBlockEntity;
+import com.github.theredbrain.bamcore.entity.player.DuckPlayerEntityMixin;
+import com.github.theredbrain.bamcore.registry.ComponentsRegistry;
 import com.github.theredbrain.bamcore.registry.EntityRegistry;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
@@ -8,6 +11,10 @@ import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -33,4 +40,16 @@ public class HousingBlock extends BlockWithEntity {
         return BlockRenderType.MODEL;
     }
 
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof HousingBlockBlockEntity housingBlockBlockEntity && player.isCreative()) {
+            ComponentsRegistry.CURRENT_HOUSING_BLOCK_POS.get(player).setValue(housingBlockBlockEntity.getPos());
+            if (world.isClient) {
+                ((DuckPlayerEntityMixin) player).bamcore$openHousingScreen();
+                return ActionResult.SUCCESS;
+            }
+        }
+        return ActionResult.PASS;
+    }
 }
