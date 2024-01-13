cb8178861c13b6ccdcd0133038cc48e91616b16c
TheRedBrain <thetrueredbrain@gmail.com>
2024-01-13 16:09:20 +0100

    triggered blocks -> script blocks

--- src/main/java/com/github/theredbrain/betteradventuremode/registry/ItemGroupRegistry.java
+++ src/main/java/com/github/theredbrain/betteradventuremode/registry/ItemGroupRegistry.java
@@ -14,6 +14,7 @@ public class ItemGroupRegistry {
     public static final RegistryKey<ItemGroup> BAM_BLOCK = RegistryKey.of(RegistryKeys.ITEM_GROUP, BetterAdventureMode.identifier("bam_block"));
     public static final RegistryKey<ItemGroup> BAM_EQUIPMENT = RegistryKey.of(RegistryKeys.ITEM_GROUP, BetterAdventureMode.identifier("bam_equipment"));
     public static final RegistryKey<ItemGroup> BAM_FOOD = RegistryKey.of(RegistryKeys.ITEM_GROUP, BetterAdventureMode.identifier("bam_food"));
+    public static final RegistryKey<ItemGroup> SCRIPT_BLOCKS = RegistryKey.of(RegistryKeys.ITEM_GROUP, BetterAdventureMode.identifier("script_blocks"));
 
     public static void init() {
         Registry.register(Registries.ITEM_GROUP, BAM_BLOCK, FabricItemGroup.builder()
@@ -27,6 +28,10 @@ public class ItemGroupRegistry {
         Registry.register(Registries.ITEM_GROUP, BAM_FOOD, FabricItemGroup.builder()
                 .icon(() -> new ItemStack(ItemRegistry.IRON_SWORD))
                 .displayName(Text.translatable("itemGroup.betteradventuremode.bam_food"))
+                .build());
+        Registry.register(Registries.ITEM_GROUP, SCRIPT_BLOCKS, FabricItemGroup.builder()
+                .icon(() -> new ItemStack(BlockRegistry.TELEPORTER_BLOCK))
+                .displayName(Text.translatable("itemGroup.betteradventuremode.script_blocks"))
                 .build());
     }
 }
