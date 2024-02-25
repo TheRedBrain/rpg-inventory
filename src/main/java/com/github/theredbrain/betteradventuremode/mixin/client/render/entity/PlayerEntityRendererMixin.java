97c329a7f86f372468751f8165ec723095734374
TheRedBrain <thetrueredbrain@gmail.com>
2024-02-25 11:02:27 +0100

    added dependency on Ranged Weapon API

--- src/main/java/com/github/theredbrain/betteradventuremode/mixin/client/render/entity/PlayerEntityRendererMixin.java
+++ src/main/java/com/github/theredbrain/betteradventuremode/mixin/client/render/entity/PlayerEntityRendererMixin.java
@@ -13,16 +13,13 @@ import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
-import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
-import net.minecraft.util.UseAction;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Environment(EnvType.CLIENT)
@@ -43,40 +40,17 @@ public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<Abs
 
     /**
      * @author TheRedBrain
-     * @reason
      */
-    @Overwrite
-    private static BipedEntityModel.ArmPose getArmPose(AbstractClientPlayerEntity player, Hand hand) {
+    @Inject(method = "getArmPose", at = @At("HEAD"), cancellable = true)
+    private static void betteradventuremode$pre_getArmPose(AbstractClientPlayerEntity player, Hand hand, CallbackInfoReturnable<BipedEntityModel.ArmPose> cir) {
         ItemStack itemStack = player.getStackInHand(hand);
         if (itemStack.isEmpty() || itemStack.isIn(Tags.EMPTY_HAND_WEAPONS)) {
-            return BipedEntityModel.ArmPose.EMPTY;
+            cir.setReturnValue(BipedEntityModel.ArmPose.EMPTY);
+            cir.cancel();
         }
-        if (player.getActiveHand() == hand && player.getItemUseTimeLeft() > 0) {
-            UseAction useAction = itemStack.getUseAction();
-            if (useAction == UseAction.BLOCK) {
-                return BipedEntityModel.ArmPose.BLOCK;
-            }
-            if (useAction == UseAction.BOW) {
-                return BipedEntityModel.ArmPose.BOW_AND_ARROW;
-            }
-            if (useAction == UseAction.SPEAR) {
-                return BipedEntityModel.ArmPose.THROW_SPEAR;
-            }
-            if (useAction == UseAction.CROSSBOW && hand == player.getActiveHand()) {
-                return BipedEntityModel.ArmPose.CROSSBOW_CHARGE;
-            }
-            if (useAction == UseAction.SPYGLASS) {
-                return BipedEntityModel.ArmPose.SPYGLASS;
-            }
-            if (useAction == UseAction.TOOT_HORN) {
-                return BipedEntityModel.ArmPose.TOOT_HORN;
-            }
-            if (useAction == UseAction.BRUSH) {
-                return BipedEntityModel.ArmPose.BRUSH;
-            }
-        } else if (!player.handSwinging && ((itemStack.isOf(Items.CROSSBOW) && CrossbowItem.isCharged(itemStack)) || player.hasStatusEffect(StatusEffectsRegistry.TWO_HANDED_EFFECT))) {
-            return BipedEntityModel.ArmPose.CROSSBOW_HOLD;
+        if (!player.handSwinging && player.hasStatusEffect(StatusEffectsRegistry.TWO_HANDED_EFFECT)) {
+            cir.setReturnValue(BipedEntityModel.ArmPose.CROSSBOW_HOLD);
+            cir.cancel();
         }
-        return BipedEntityModel.ArmPose.ITEM;
     }
 }
