cf98c080b51a70173360919b64d243218da155be
TheRedBrain <thetrueredbrain@gmail.com>
2024-07-15 14:00:04 +0200

    added icon and fixed item usability check

--- src/main/java/com/github/theredbrain/rpginventory/util/ItemUtils.java
+++ src/main/java/com/github/theredbrain/rpginventory/util/ItemUtils.java
@@ -5,10 +5,10 @@ import net.minecraft.item.ItemStack;
 
 public class ItemUtils {
 
-    /**
-     * @return whether this item should provide its attribute modifiers and if it should be rendered
-     */
-    public static boolean isUsable(ItemStack stack) {
-        return stack.getDamage() < stack.getMaxDamage() - 1 || stack.isIn(Tags.EMPTY_HAND_WEAPONS) || stack.isIn(Tags.UNUSABLE_WHEN_LOW_DURABILITY);
-    }
+	/**
+	 * @return whether this item should provide its attribute modifiers and if it should be rendered
+	 */
+	public static boolean isUsable(ItemStack stack) {
+		return stack.getDamage() < stack.getMaxDamage() - 1 || stack.isIn(Tags.EMPTY_HAND_WEAPONS) || !stack.isIn(Tags.UNUSABLE_WHEN_LOW_DURABILITY);
+	}
 }
