b59d9864f96ead782ffa35afdfbd8ed72a5b459b
TheRedBrain <thetrueredbrain@gmail.com>
2025-05-06 11:10:24 +0200

    added custom AttributeModifierSlots

---
+++ src/main/java/com/github/theredbrain/rpginventory/component/type/ExtendedAttributeModifierSlot.java
@@ -0,0 +1,20 @@
+package com.github.theredbrain.rpginventory.component.type;
+
+import net.minecraft.component.type.AttributeModifierSlot;
+
+public class ExtendedAttributeModifierSlot {
+	public static AttributeModifierSlot BELT = AttributeModifierSlot.valueOf("BELT");
+	public static AttributeModifierSlot GLOVES = AttributeModifierSlot.valueOf("GLOVES");
+	public static AttributeModifierSlot NECKLACE = AttributeModifierSlot.valueOf("NECKLACE");
+	public static AttributeModifierSlot RING_1 = AttributeModifierSlot.valueOf("RING_1");
+	public static AttributeModifierSlot RING_2 = AttributeModifierSlot.valueOf("RING_2");
+	public static AttributeModifierSlot SHOULDERS = AttributeModifierSlot.valueOf("SHOULDERS");
+	public static AttributeModifierSlot SPELL_1 = AttributeModifierSlot.valueOf("SPELL_1");
+	public static AttributeModifierSlot SPELL_2 = AttributeModifierSlot.valueOf("SPELL_2");
+	public static AttributeModifierSlot SPELL_3 = AttributeModifierSlot.valueOf("SPELL_3");
+	public static AttributeModifierSlot SPELL_4 = AttributeModifierSlot.valueOf("SPELL_4");
+	public static AttributeModifierSlot SPELL_5 = AttributeModifierSlot.valueOf("SPELL_5");
+	public static AttributeModifierSlot SPELL_6 = AttributeModifierSlot.valueOf("SPELL_6");
+	public static AttributeModifierSlot SPELL_7 = AttributeModifierSlot.valueOf("SPELL_7");
+	public static AttributeModifierSlot SPELL_8 = AttributeModifierSlot.valueOf("SPELL_8");
+}
