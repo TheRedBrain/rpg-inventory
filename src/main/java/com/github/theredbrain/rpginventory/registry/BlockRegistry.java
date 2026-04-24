c36854057a26649263df2aea6246005244cc6568
TheRedBrain <thetrueredbrain@gmail.com>
2026-04-24 11:55:31 +0200

    fix block and item registry

--- src/main/java/com/github/theredbrain/rpginventory/registry/BlockRegistry.java
+++ src/main/java/com/github/theredbrain/rpginventory/registry/BlockRegistry.java
@@ -5,6 +5,7 @@ import com.github.theredbrain.rpginventory.block.MannequinBlock;
 import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -17,15 +18,20 @@ import net.minecraft.world.level.material.MapColor;
 import java.util.List;
 
 public class BlockRegistry {
+	public static ResourceKey<Block> MANNEQUIN_BLOCK_KEY = ResourceKey.create(Registries.BLOCK, RPGInventory.identifier("mannequin"));
+	public static ResourceKey<Item> MANNEQUIN_ITEM_KEY = ResourceKey.create(Registries.ITEM, RPGInventory.identifier("mannequin"));
 
-	public static final Block MANNEQUIN = registerBlock("mannequin", new MannequinBlock(BlockBehaviour.Properties.of().mapColor(MapColor.WOOD).strength(10.0F, 3600000.0f).noOcclusion()), List.of(CreativeModeTabs.FUNCTIONAL_BLOCKS));
+	public static final Block MANNEQUIN = registerBlock(MANNEQUIN_BLOCK_KEY, MANNEQUIN_ITEM_KEY, new MannequinBlock(BlockBehaviour.Properties.of().setId(MANNEQUIN_BLOCK_KEY).mapColor(MapColor.WOOD).strength(10.0F, 3600000.0f).noOcclusion()), List.of(CreativeModeTabs.FUNCTIONAL_BLOCKS));
 
-	private static Block registerBlock(String name, Block block, List<ResourceKey<CreativeModeTab>> creativeModeTabList) {
-		Registry.register(BuiltInRegistries.ITEM, RPGInventory.identifier(name), new BlockItem(block, new Item.Properties()));
-		for (ResourceKey<CreativeModeTab> creativeModeTabResourceKey : creativeModeTabList) {
-			CreativeModeTabEvents.modifyOutputEvent(creativeModeTabResourceKey).register(content -> content.accept(block));
+	private static Block registerBlock(ResourceKey<Block> block_key, ResourceKey<Item> item_key, Block block, List<ResourceKey<CreativeModeTab>> creativeModeTabList, Block... alternatives) {
+		Item blockItem = Registry.register(BuiltInRegistries.ITEM, item_key, new BlockItem(block, new Item.Properties().setId(item_key).fireResistant()));
+		for (ResourceKey<CreativeModeTab> creativeModeTab : creativeModeTabList) {
+			CreativeModeTabEvents.modifyOutputEvent(creativeModeTab).register(content -> content.accept(block));
 		}
-		return Registry.register(BuiltInRegistries.BLOCK, RPGInventory.identifier(name), block);
+		for (Block alternative : alternatives) {
+			Item.BY_BLOCK.put(alternative, blockItem);
+		}
+		return Registry.register(BuiltInRegistries.BLOCK, block_key, block);
 	}
 
 	public static void init() {
--- src/main/java/com/github/theredbrain/rpginventory/registry/ItemRegistry.java
+++ src/main/java/com/github/theredbrain/rpginventory/registry/ItemRegistry.java
@@ -5,6 +5,7 @@ import com.github.theredbrain.rpginventory.item.EmptyHandWeapon;
 import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -13,14 +14,18 @@ import java.util.List;
 
 public class ItemRegistry {
 
-	public static final Item DEFAULT_EMPTY_HAND_WEAPON = registerItem("default_empty_hand_weapon", new EmptyHandWeapon(new Item.Properties().stacksTo(1)), List.of());
+	public static ResourceKey<Item> DEFAULT_EMPTY_HAND_WEAPON_KEY = ResourceKey.create(Registries.ITEM, RPGInventory.identifier("default_empty_hand_weapon"));
+	public static final Item DEFAULT_EMPTY_HAND_WEAPON = registerItem(DEFAULT_EMPTY_HAND_WEAPON_KEY, new EmptyHandWeapon(new Item.Properties().setId(DEFAULT_EMPTY_HAND_WEAPON_KEY).stacksTo(1)), List.of());
 
-	private static Item registerItem(String name, Item item, List<ResourceKey<CreativeModeTab>> creativeModeTabList) {
+	private static Item registerItem(ResourceKey<Item> key, Item item, List<ResourceKey<CreativeModeTab>> creativeModeTabList) {
 
-		for (ResourceKey<CreativeModeTab> creativeModeTabResourceKey : creativeModeTabList) {
-			CreativeModeTabEvents.modifyOutputEvent(creativeModeTabResourceKey).register(content -> content.accept(item));
+		for (ResourceKey<CreativeModeTab> creativeModeTab : creativeModeTabList) {
+			CreativeModeTabEvents.modifyOutputEvent(creativeModeTab).register(content -> {
+				content.accept(item);
+			});
 		}
-		return Registry.register(BuiltInRegistries.ITEM, ResourceKey.create(BuiltInRegistries.ITEM.key(), RPGInventory.identifier(name)), item);
+
+		return Registry.register(BuiltInRegistries.ITEM, key, item);
 	}
 
 	public static void init() {
