42411bdd31cc3165a1f50b61b4725c8e51c653cb
TheRedBrain <thetrueredbrain@gmail.com>
2026-04-29 15:18:16 +0200

    removed dedicated EmptyHandWeapon class

--- src/main/java/com/github/theredbrain/rpginventory/registry/ItemRegistry.java
+++ src/main/java/com/github/theredbrain/rpginventory/registry/ItemRegistry.java
@@ -1,12 +1,13 @@
 package com.github.theredbrain.rpginventory.registry;
 
 import com.github.theredbrain.rpginventory.RPGInventory;
-import com.github.theredbrain.rpginventory.item.EmptyHandWeapon;
 import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.Unit;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 
@@ -15,7 +16,7 @@ import java.util.List;
 public class ItemRegistry {
 
 	public static ResourceKey<Item> DEFAULT_EMPTY_HAND_WEAPON_KEY = ResourceKey.create(Registries.ITEM, RPGInventory.identifier("default_empty_hand_weapon"));
-	public static final Item DEFAULT_EMPTY_HAND_WEAPON = registerItem(DEFAULT_EMPTY_HAND_WEAPON_KEY, new EmptyHandWeapon(new Item.Properties().setId(DEFAULT_EMPTY_HAND_WEAPON_KEY).stacksTo(1)), List.of());
+	public static final Item DEFAULT_EMPTY_HAND_WEAPON = registerItem(DEFAULT_EMPTY_HAND_WEAPON_KEY, new Item(new Item.Properties().setId(DEFAULT_EMPTY_HAND_WEAPON_KEY).component(DataComponents.UNBREAKABLE, Unit.INSTANCE).stacksTo(1)), List.of());
 
 	private static Item registerItem(ResourceKey<Item> key, Item item, List<ResourceKey<CreativeModeTab>> creativeModeTabList) {
 
