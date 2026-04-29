0c12068f865c7533e52c97dc65d89d50dddbd4cc
TheRedBrain <thetrueredbrain@gmail.com>
2026-04-29 14:35:59 +0200

    fixed hand slot actions

--- src/main/java/com/github/theredbrain/rpginventory/network/packet/SheatheWeaponsPacketReceiver.java
+++ src/main/java/com/github/theredbrain/rpginventory/network/packet/SheatheWeaponsPacketReceiver.java
@@ -24,50 +24,140 @@ public class SheatheWeaponsPacketReceiver implements ServerPlayNetworking.PlayPa
 
 			ServerConfig serverConfig = RPGInventory.SERVER_CONFIG;
 
-			ItemStack handItemStack = player.getItemBySlot(EquipmentSlot.MAINHAND).copy();
-			if (handItemStack.isEmpty()) {
-				handItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_HAND).copy();
-			}
-			ItemStack offHandItemStack = player.getItemBySlot(EquipmentSlot.OFFHAND).copy();
-			if (offHandItemStack.isEmpty()) {
-				offHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND).copy();
-			}
-
-			if (RPGInventory.doesCurrentPlayerStatusPreventHandSlotAction(player) ||
-					player.getCooldowns().isOnCooldown(handItemStack) ||
-					player.getCooldowns().isOnCooldown(offHandItemStack)
-			) {
-				player.sendSystemMessage(Component.translatable("hud.message.handSlotActionWasPrevented"), true);
-				return;
-			}
-
 			float staminaCost = RPGInventory.isStaminaAttributesLoaded ? serverConfig.handSlotOverhaul.staminaAttributesCompat.sheathing_hand_items_stamina_cost.get() : 0.0F;
 
 			if (staminaCost > 0.0F && !player.isCreative() && serverConfig.handSlotOverhaul.staminaAttributesCompat.sheathing_hand_items_requires_stamina.get() && RPGInventory.getCurrentStamina(player) <= 0 && (!serverConfig.handSlotOverhaul.staminaAttributesCompat.sheathing_hand_items_requires_stamina_cost.get() || RPGInventory.getCurrentStamina(player) < staminaCost)) {
 				player.sendSystemMessage(Component.translatable("hud.message.staminaTooLow"), true);
 				return;
 			}
+
+			boolean playerStatusPreventsHandSlotAction = RPGInventory.doesCurrentPlayerStatusPreventHandSlotAction(player);
+			ItemStack handItemStack;
+			ItemStack sheathedHandItemStack;
+			ItemStack offHandItemStack;
+			ItemStack sheathedOffHandItemStack;
+
 			if (((DuckLivingEntityMixin) player).rpginventory$isHandStackSheathed() && ((DuckLivingEntityMixin) player).rpginventory$isOffhandStackSheathed()) {
 				((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(false);
 				((DuckLivingEntityMixin) player).rpginventory$setIsOffhandStackSheathed(false);
-				player.setItemSlot(EquipmentSlot.MAINHAND, handItemStack);
-				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_HAND, ItemStack.EMPTY);
-				player.setItemSlot(EquipmentSlot.OFFHAND, offHandItemStack);
-				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND, ItemStack.EMPTY);
+
+				handItemStack = player.getItemBySlot(EquipmentSlot.MAINHAND).copy();
+				sheathedHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_HAND).copy();
+				if (!handItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Hand Slot while their hand was sheathed. This should not happen!");
+				}
+
+				offHandItemStack = player.getItemBySlot(EquipmentSlot.OFFHAND).copy();
+				sheathedOffHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND).copy();
+				if (!offHandItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Offhand Slot while their offhand was sheathed. This should not happen!");
+				}
+
+				if (playerStatusPreventsHandSlotAction ||
+						player.getCooldowns().isOnCooldown(sheathedHandItemStack) ||
+						player.getCooldowns().isOnCooldown(sheathedOffHandItemStack)
+				) {
+					((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(true);
+					((DuckLivingEntityMixin) player).rpginventory$setIsOffhandStackSheathed(true);
+					player.sendSystemMessage(Component.translatable("hud.message.handSlotActionWasPrevented"), true);
+					return;
+				}
+
+				player.setItemSlot(EquipmentSlot.MAINHAND, sheathedHandItemStack);
+				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_HAND, handItemStack);
+
+				player.setItemSlot(EquipmentSlot.OFFHAND, sheathedOffHandItemStack);
+				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND, offHandItemStack);
+
+				if (serverConfig.handSlotOverhaul.enable_item_cooldown_after_hand_sheathing.get()) {
+					player.getCooldowns().addCooldown(sheathedHandItemStack, serverConfig.handSlotOverhaul.sheathing_main_hand_cooldown.get());
+					player.getCooldowns().addCooldown(sheathedOffHandItemStack, serverConfig.handSlotOverhaul.sheathing_offhand_cooldown.get());
+				}
+			} else if (((DuckLivingEntityMixin) player).rpginventory$isHandStackSheathed()) {
+				// this should never happen
+				RPGInventory.info(player.getName() + "'s hand was sheathed, while their offhand was not sheathed. This should not happen!");
+
+				((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(false);
+
+				handItemStack = player.getItemBySlot(EquipmentSlot.MAINHAND).copy();
+				sheathedHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_HAND).copy();
+				if (!handItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Hand Slot while their hand was sheathed. This should not happen!");
+				}
+
+				if (playerStatusPreventsHandSlotAction ||
+						player.getCooldowns().isOnCooldown(sheathedHandItemStack)
+				) {
+					((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(true);
+					player.sendSystemMessage(Component.translatable("hud.message.handSlotActionWasPrevented"), true);
+					return;
+				}
+
+				player.setItemSlot(EquipmentSlot.MAINHAND, sheathedHandItemStack);
+				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_HAND, handItemStack);
+
+				if (serverConfig.handSlotOverhaul.enable_item_cooldown_after_hand_sheathing.get()) {
+					player.getCooldowns().addCooldown(sheathedHandItemStack, serverConfig.handSlotOverhaul.sheathing_main_hand_cooldown.get());
+				}
+			} else if (((DuckLivingEntityMixin) player).rpginventory$isOffhandStackSheathed()) {
+				handItemStack = player.getItemBySlot(EquipmentSlot.MAINHAND).copy();
+				sheathedHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_HAND).copy();
+				if (!sheathedHandItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Sheathed Hand Slot while their hand was not sheathed. This should not happen!");
+				}
+
+				if (playerStatusPreventsHandSlotAction ||
+						player.getCooldowns().isOnCooldown(handItemStack)
+				) {
+					player.sendSystemMessage(Component.translatable("hud.message.handSlotActionWasPrevented"), true);
+					return;
+				}
+
+				player.setItemSlot(EquipmentSlot.MAINHAND, sheathedHandItemStack);
+				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_HAND, handItemStack);
+
+				if (serverConfig.handSlotOverhaul.enable_item_cooldown_after_toggling_2_handed_stance.get()) {
+					player.getCooldowns().addCooldown(sheathedHandItemStack, serverConfig.handSlotOverhaul.toggling_2_handed_stance_main_hand_cooldown.get());
+				}
+				((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(true);
 			} else {
+				handItemStack = player.getItemBySlot(EquipmentSlot.MAINHAND).copy();
+				sheathedHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_HAND).copy();
+				if (!sheathedHandItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Sheathed Hand Slot while their hand was not sheathed. This should not happen!");
+				}
+
+				offHandItemStack = player.getItemBySlot(EquipmentSlot.OFFHAND).copy();
+				sheathedOffHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND).copy();
+				if (!sheathedOffHandItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Sheathed Offhand Slot while their offhand was not sheathed. This should not happen!");
+				}
+
+				if (playerStatusPreventsHandSlotAction ||
+						player.getCooldowns().isOnCooldown(handItemStack) ||
+						player.getCooldowns().isOnCooldown(offHandItemStack)
+				) {
+					player.sendSystemMessage(Component.translatable("hud.message.handSlotActionWasPrevented"), true);
+					return;
+				}
+
+				player.setItemSlot(EquipmentSlot.MAINHAND, sheathedHandItemStack);
+				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_HAND, handItemStack);
+
+				player.setItemSlot(EquipmentSlot.OFFHAND, sheathedOffHandItemStack);
+				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND, offHandItemStack);
+
+				if (serverConfig.handSlotOverhaul.enable_item_cooldown_after_hand_sheathing.get()) {
+					player.getCooldowns().addCooldown(handItemStack, serverConfig.handSlotOverhaul.sheathing_main_hand_cooldown.get());
+					player.getCooldowns().addCooldown(offHandItemStack, serverConfig.handSlotOverhaul.sheathing_offhand_cooldown.get());
+				}
+
 				((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(true);
 				((DuckLivingEntityMixin) player).rpginventory$setIsOffhandStackSheathed(true);
-				player.setItemSlot(EquipmentSlot.MAINHAND, ItemStack.EMPTY);
-				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_HAND, handItemStack);
-				player.setItemSlot(EquipmentSlot.OFFHAND, ItemStack.EMPTY);
-				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND, offHandItemStack);
 			}
+
 			if (staminaCost != 0.0F && !player.isCreative()) {
 				RPGInventory.addStamina(player, -staminaCost);
-			}
-			if (serverConfig.handSlotOverhaul.enable_item_cooldown_after_hand_sheathing.get()) {
-				player.getCooldowns().addCooldown(player.getMainHandItem(), serverConfig.handSlotOverhaul.sheathing_main_hand_cooldown.get());
-				player.getCooldowns().addCooldown(player.getOffhandItem(), serverConfig.handSlotOverhaul.sheathing_offhand_cooldown.get());
 			}
 			player.level().playSound(null, player.blockPosition().getX(), player.blockPosition().getY(), player.blockPosition().getZ(), SoundEvents.ARMOR_EQUIP_GENERIC, SoundSource.PLAYERS, 1.0F, 1.0F);
 		} else {
--- src/main/java/com/github/theredbrain/rpginventory/network/packet/ToggleTwoHandedStancePacketReceiver.java
+++ src/main/java/com/github/theredbrain/rpginventory/network/packet/ToggleTwoHandedStancePacketReceiver.java
@@ -24,62 +24,138 @@ public class ToggleTwoHandedStancePacketReceiver implements ServerPlayNetworking
 
 			ServerConfig serverConfig = RPGInventory.SERVER_CONFIG;
 
-			ItemStack handItemStack = player.getItemBySlot(EquipmentSlot.MAINHAND).copy();
-			if (handItemStack.isEmpty()) {
-				handItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_HAND).copy();
-			}
-			ItemStack offHandItemStack = player.getItemBySlot(EquipmentSlot.OFFHAND).copy();
-			if (offHandItemStack.isEmpty()) {
-				offHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND).copy();
-			}
-
-			if (RPGInventory.doesCurrentPlayerStatusPreventHandSlotAction(player) ||
-					player.getCooldowns().isOnCooldown(handItemStack) ||
-					player.getCooldowns().isOnCooldown(offHandItemStack)
-			) {
-				player.sendSystemMessage(Component.translatable("hud.message.handSlotActionWasPrevented"), true);
-				return;
-			}
-
-			boolean mainHandCanNotBeTwoHanded = handItemStack.is(Tags.NON_TWO_HANDED_ITEMS);
-
 			float staminaCost = RPGInventory.isStaminaAttributesLoaded ? serverConfig.handSlotOverhaul.staminaAttributesCompat.toggling_two_handed_stance_stamina_cost.get() : 0.0F;
 
 			if (staminaCost > 0.0F && !player.isCreative() && serverConfig.handSlotOverhaul.staminaAttributesCompat.toggling_two_handed_stance_requires_stamina.get() && RPGInventory.getCurrentStamina(player) <= 0 && (!serverConfig.handSlotOverhaul.staminaAttributesCompat.toggling_two_handed_stance_requires_stamina_cost.get() || RPGInventory.getCurrentStamina(player) < staminaCost)) {
 				player.sendSystemMessage(Component.translatable("hud.message.staminaTooLow"), true);
 				return;
-			} else if (((DuckLivingEntityMixin) player).rpginventory$isHandStackSheathed() && ((DuckLivingEntityMixin) player).rpginventory$isOffhandStackSheathed()) {
+			}
+
+			boolean playerStatusPreventsHandSlotAction = RPGInventory.doesCurrentPlayerStatusPreventHandSlotAction(player);
+			ItemStack handItemStack;
+			ItemStack sheathedHandItemStack;
+			ItemStack offHandItemStack;
+			ItemStack sheathedOffHandItemStack;
+
+			if (((DuckLivingEntityMixin) player).rpginventory$isHandStackSheathed() && ((DuckLivingEntityMixin) player).rpginventory$isOffhandStackSheathed()) {
 				if (!serverConfig.handSlotOverhaul.always_allow_toggling_two_handed_stance.get()) {
 					player.sendSystemMessage(Component.translatable("hud.message.weaponsAreSheathed"), true);
 					return;
-				} else {
-					if (mainHandCanNotBeTwoHanded) {
+				}
+
+				((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(false);
+
+				handItemStack = player.getItemBySlot(EquipmentSlot.MAINHAND).copy();
+				sheathedHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_HAND).copy();
+				if (!handItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Hand Slot while their hand was sheathed. This should not happen!");
+				}
+				if (handItemStack.is(Tags.NON_TWO_HANDED_ITEMS)) {
+					((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(true);
 						player.sendSystemMessage(Component.translatable("hud.message.nonTwoHandedWeaponEquipped"), true);
 						return;
 					}
-					((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(false);
-					player.setItemSlot(EquipmentSlot.MAINHAND, handItemStack);
-					player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_HAND, ItemStack.EMPTY);
+
+				if (playerStatusPreventsHandSlotAction ||
+						player.getCooldowns().isOnCooldown(sheathedHandItemStack)
+				) {
+					((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(true);
+					player.sendSystemMessage(Component.translatable("hud.message.handSlotActionWasPrevented"), true);
+					return;
+				}
+
+				player.setItemSlot(EquipmentSlot.MAINHAND, sheathedHandItemStack);
+					player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_HAND, handItemStack);
+
+				if (serverConfig.handSlotOverhaul.enable_item_cooldown_after_toggling_2_handed_stance.get()) {
+					player.getCooldowns().addCooldown(sheathedHandItemStack, serverConfig.handSlotOverhaul.toggling_2_handed_stance_main_hand_cooldown.get());
+				}
+			} else if (((DuckLivingEntityMixin) player).rpginventory$isHandStackSheathed()) {
+				// this should never happen
+				RPGInventory.info(player.getName() + "'s hand was sheathed, while their offhand was not sheathed. This should not happen!");
+
+				((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(false);
+
+				handItemStack = player.getItemBySlot(EquipmentSlot.MAINHAND).copy();
+				sheathedHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_HAND).copy();
+				if (!handItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Hand Slot while their hand was sheathed. This should not happen!");
+				}
+
+				if (playerStatusPreventsHandSlotAction ||
+						player.getCooldowns().isOnCooldown(sheathedHandItemStack)
+				) {
+					((DuckLivingEntityMixin) player).rpginventory$setIsHandStackSheathed(true);
+					player.sendSystemMessage(Component.translatable("hud.message.handSlotActionWasPrevented"), true);
+					return;
+				}
+
+				player.setItemSlot(EquipmentSlot.MAINHAND, sheathedHandItemStack);
+				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_HAND, handItemStack);
+
+				if (serverConfig.handSlotOverhaul.enable_item_cooldown_after_toggling_2_handed_stance.get()) {
+					player.getCooldowns().addCooldown(sheathedHandItemStack, serverConfig.handSlotOverhaul.toggling_2_handed_stance_main_hand_cooldown.get());
 				}
 			} else if (((DuckLivingEntityMixin) player).rpginventory$isOffhandStackSheathed()) {
 				((DuckLivingEntityMixin) player).rpginventory$setIsOffhandStackSheathed(false);
-				player.setItemSlot(EquipmentSlot.OFFHAND, offHandItemStack);
-				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND, ItemStack.EMPTY);
+
+				offHandItemStack = player.getItemBySlot(EquipmentSlot.OFFHAND).copy();
+				sheathedOffHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND).copy();
+				if (!offHandItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Offhand Slot while their offhand was sheathed. This should not happen!");
+				}
+
+				if (playerStatusPreventsHandSlotAction ||
+						player.getCooldowns().isOnCooldown(sheathedOffHandItemStack)
+				) {
+					((DuckLivingEntityMixin) player).rpginventory$setIsOffhandStackSheathed(true);
+					player.sendSystemMessage(Component.translatable("hud.message.handSlotActionWasPrevented"), true);
+					return;
+				}
+
+				player.setItemSlot(EquipmentSlot.OFFHAND, sheathedOffHandItemStack);
+				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND, offHandItemStack);
+
+				if (serverConfig.handSlotOverhaul.enable_item_cooldown_after_toggling_2_handed_stance.get()) {
+					player.getCooldowns().addCooldown(sheathedOffHandItemStack, serverConfig.handSlotOverhaul.toggling_2_handed_stance_offhand_cooldown.get());
+				}
 			} else {
-				if (mainHandCanNotBeTwoHanded) {
+				handItemStack = player.getItemBySlot(EquipmentSlot.MAINHAND).copy();
+				sheathedHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_HAND).copy();
+				if (!sheathedHandItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Sheathed Hand Slot while their hand was not sheathed. This should not happen!");
+				}
+
+				offHandItemStack = player.getItemBySlot(EquipmentSlot.OFFHAND).copy();
+				sheathedOffHandItemStack = player.getItemBySlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND).copy();
+				if (!sheathedOffHandItemStack.equals(ItemStack.EMPTY)) {
+					RPGInventory.info(player.getName() + " had an item in their Sheathed Offhand Slot while their offhand was not sheathed. This should not happen!");
+				}
+
+				if (handItemStack.is(Tags.NON_TWO_HANDED_ITEMS)) {
 					player.sendSystemMessage(Component.translatable("hud.message.nonTwoHandedWeaponEquipped"), true);
 					return;
 				}
-				((DuckLivingEntityMixin) player).rpginventory$setIsOffhandStackSheathed(true);
-				player.setItemSlot(EquipmentSlot.OFFHAND, ItemStack.EMPTY);
+
+				if (playerStatusPreventsHandSlotAction ||
+						player.getCooldowns().isOnCooldown(offHandItemStack)
+				) {
+					player.sendSystemMessage(Component.translatable("hud.message.handSlotActionWasPrevented"), true);
+					return;
+				}
+
+				player.setItemSlot(EquipmentSlot.OFFHAND, sheathedOffHandItemStack);
 				player.setItemSlot(ExtendedEquipmentSlot.SHEATHED_OFF_HAND, offHandItemStack);
+
+				if (serverConfig.handSlotOverhaul.enable_item_cooldown_after_toggling_2_handed_stance.get()) {
+					player.getCooldowns().addCooldown(offHandItemStack, serverConfig.handSlotOverhaul.toggling_2_handed_stance_offhand_cooldown.get());
+				}
+
+				((DuckLivingEntityMixin) player).rpginventory$setIsOffhandStackSheathed(true);
 			}
+
 			if (staminaCost != 0.0F && !player.isCreative()) {
 				RPGInventory.addStamina(player, -staminaCost);
-			}
-			if (serverConfig.handSlotOverhaul.enable_item_cooldown_after_toggling_2_handed_stance.get()) {
-				player.getCooldowns().addCooldown(player.getMainHandItem(), serverConfig.handSlotOverhaul.toggling_2_handed_stance_main_hand_cooldown.get());
-				player.getCooldowns().addCooldown(player.getOffhandItem(), serverConfig.handSlotOverhaul.toggling_2_handed_stance_offhand_cooldown.get());
 			}
 			player.level().playSound(null, player.blockPosition().getX(), player.blockPosition().getY(), player.blockPosition().getZ(), SoundEvents.ARMOR_EQUIP_GENERIC, SoundSource.PLAYERS, 1.0F, 1.0F);
 		} else {
